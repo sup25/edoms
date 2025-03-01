e8449d9fde63f514b99ac57e40e3d8dc33e1516e
sup25 <suparnaa51@gmail.com>
2025-03-01 21:45:29 +0545

    auth-db creation and  readme update

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import sequelize from "./config/db";
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection successful");
+  } catch (error) {
+    console.error("Connection failed:", error);
+  }
+})();
