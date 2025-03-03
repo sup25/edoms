aecf4c151ccb4b483739d1b0c0a4f67190f06451
sup25 <suparnaa51@gmail.com>
2025-03-03 23:40:35 +0545

    feat: add login logic and middleware logic

--- auth-service/src/auth/route/index.ts
+++ auth-service/src/auth/route/index.ts
@@ -1,8 +1,18 @@
-import express from "express";
-import { registerUserController } from "../controller";
+import express, { Request, Response } from "express";
+import { loginUserController, registerUserController } from "../controller";
+import { authMiddleware } from "../middleware";
 
 const router = express.Router();
 
 router.post("/register", registerUserController);
+router.post("/login", loginUserController);
+
+router.get("/protected", authMiddleware, (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "You have access to this protected route!",
+    data: req.user,
+  });
+});
 
 export default router;
--- auth-service/src/auth/service/index.ts
+++ auth-service/src/auth/service/index.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import { TAuthSchema } from "../validations";
 import { generateToken } from "../utils/jwtUtils";
 import User from "../model";
+import { getExpirationFromToken } from "../utils/getExpirationFromToken";
 
 export const userRegister = async ({
   email,
@@ -23,4 +24,29 @@ export const userRegister = async ({
   const token = generateToken(user.id.toString(), user.email, user.role);
 
   return token;
+};
+
+export const userLogin = async ({
+  email,
+  password,
+}: TAuthSchema): Promise<{ token: string; expiresAt: Date | null }> => {
+  // Find user by email
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    throw new Error("Invalid credentials");
+  }
+
+  // Verify password
+  const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+  if (!isPasswordValid) {
+    throw new Error("Invalid credentials");
+  }
+
+  // Generate JWT
+  const token = generateToken(user.id.toString(), user.email, user.role);
+
+  // Get expiration date
+  const expiresAt = getExpirationFromToken(token);
+
+  return { token, expiresAt };
 };
