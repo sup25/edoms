f09beb46d687a47dc81c57055f2554335a24907e
sup25 <suparnaa51@gmail.com>
2025-03-04 13:40:16 +0545

    Add tests for authentication system

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -17,9 +17,13 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
-app.use("/api/v1/users", router);
+app.use("/api/v1/user", router);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export { app };
